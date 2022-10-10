8040205e33e351fe85112c8ea6d0d8ce80288816
cmaxreilly <c.maxwell.reilly@gmail.com>
2022-10-09 22:56:01 -0600

    Initial commit

---
+++ priority/core.py
@@ -0,0 +1,12 @@
+import data
+import helpers
+
+sorted_alphabetically = sorted(data.data)
+
+
+# Test print
+
+print(helpers.creatArray(sorted_alphabetically))
+
+
+ 
---
+++ priority/helpers.py
@@ -0,0 +1,10 @@
+# Function that takes a list and outputs an array with every unique entry coupled with the number of times it occurs in the list.
+# Ex: list = [1, 2, 3, 3, 4, 5, 5, 5,]
+# print(createDict(list)) = [[1, 1], [2, 1], [3, 2], [4, 1], [5, 3]]
+def creatArray(list):
+    array = []
+    for entry in list:
+        pass
+    return(array)
+
+# Create a function that writes new data to the data.py thing
