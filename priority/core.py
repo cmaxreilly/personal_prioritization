7931dc8b911aa7c5cb0ab9c314e6d0a5add89896
cmaxreilly <c.maxwell.reilly@gmail.com>
2022-11-08 10:39:26 -0700

    Added the worry function

--- priority/core.py
+++ priority/core.py
@@ -1,12 +1,25 @@
 import data
 import helpers
+import os
+import sys
 
 sorted_alphabetically = sorted(data.data)
 
 
 # Test print
 
-print(helpers.creatArray(sorted_alphabetically))
+# print(helpers.creatArray(sorted_alphabetically))
 
+# if the number of arguments is 1:
+    # determine the number of each item in the list of worries
+    # use this number to create a list of priorities in decending order
+if len(sys.argv) == 1:
+    print(data.data)
 
+if len(sys.argv)== 2:
+    if sys.argv[1] == "-d": # When script is called with -d:
+        dat = open("data.py", "a") # open data.py in append mode as dat
+        new_entry = str(input("what data would you like to input?\n")) # Ask "What data would you like to input?"
+        dat.write("\ndata.append('{}')".format(new_entry)) # append code that writes data to list into dat
+        dat.close() # close dat
  
--- priority/data.py
+++ priority/data.py
@@ -1,3 +1,4 @@
 data = ["invoices", "shopping", "dishes", "checks", "files", "st kitts", "car", "clothes", "room organization", "cooking", "seqouia", "kyle", "amazon packages", "tmlj", "umesh", "dishes", "songwriting", "schumann concerto", "social media", "coursera", "jerusalem", "unfinished books", "haircut", "fence", "repair manual", "therapy", "bella", "lilly", "steve texting", "collapse", "ukraine", "kurds", "gym subscription", "scheduling", "coursera", "miranda","treasure island", "shopping", "files", "ra", "gym", "ra", "haircut", "files", "car", "ra"]
+data.append('contracts')
 
-
+data.append('therapy')
